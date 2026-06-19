e9eae69cc0dec64f994a4e1d57e0053900cffd30
JustinProj22 <justin@project22.com.au>
2026-06-19 12:00:10 +1000

    Update app.js

--- Chat/app.js
+++ Chat/app.js
@@ -1,7 +1,7 @@
 // ============================================================
 // CONFIG — fill these in
 // ============================================================
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzSVyJB8EhdqpCbLXv2YqZudenHBtol3Bqpa7zG-cwldlWIxXbzJwvw19MDRQIugRpp/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbytisOmSlyD-8nEjt66rPbF0SFT4Yc4o2A0K9sWIApsb-KjLoNpSOPbyj7Ke950-QbE/exec';
 const VAPID_PUBLIC_KEY = 'BPgI6fOwbKkvNXU_UG_SO3xYlhGsB1QMfFHNPf6yhPFF3P_ck7zNypzb_iwL8HPYeEzwfAHUuVrw39WCN3Y-ZU8';
 const GIPHY_API_KEY    = 'X6uHs1HdNBeFDtnk8dHJWlXUk108UFbj';
 
