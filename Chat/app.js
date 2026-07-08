f457fd27b9f30aa3c1dc8099f1d76dc9ec9b8b17
JustinProj22 <justin@project22.com.au>
2026-07-08 11:53:57 +1000

    Deployment Update

--- Chat/app.js
+++ Chat/app.js
@@ -1,7 +1,7 @@
 // ============================================================
 // CONFIG — fill these in
 // ============================================================
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyhU3_zRjtR_tGbV39gSa43AvvKWftGYxiRKCK2lweFd20XrW7W2noH9yyWgvn4wdlw/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzenbeRf83yjgnscJIJrW9oebFhbmgHk1jbIVpWhALFFjQa97W6mTY8KKrAgGASsffm/exec';
 const VAPID_PUBLIC_KEY = 'BPgI6fOwbKkvNXU_UG_SO3xYlhGsB1QMfFHNPf6yhPFF3P_ck7zNypzb_iwL8HPYeEzwfAHUuVrw39WCN3Y-ZU8';
 const GIPHY_API_KEY    = 'X6uHs1HdNBeFDtnk8dHJWlXUk108UFbj';
 
